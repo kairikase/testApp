528438ed0182b73ce40a689088bde10df46ba7d6
Kairi Kase <kasekairi@gmail.com>
2018-02-25 15:46:01 +0200

    lisasime highscores ja ruuteri seadistuse

--- src/app.js
+++ src/app.js
@@ -2,4 +2,13 @@ export class App {
   constructor() {
     this.message = 'Teretulemast meie rakendusse!';
   }
+  
+  configureRouter(config, router) {
+        this.router = router;
+        config.title = 'Meie Aurelia test App';
+        config.map([
+          { route: ['', 'home'], name: 'home',       moduleId: 'home/index' },
+          { route: 'highscores', name: 'highscores',      moduleId: 'highscores', nav: true}
+        ]);
+      }
 }
---
+++ src/highscores.js
@@ -0,0 +1,8 @@
+export class highscores {
+  constructor() {
+	this.appName = "testApp"
+	this.count=0
+    this.message = 'Siin on mäkumängu tulemused kirjas!'
+  }
+  
+}
